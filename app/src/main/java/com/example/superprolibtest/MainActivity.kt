f8422c67b2e0280cf4be1d5e9b826d826c6d4f3b
ravitejkumar <g.ravitejkumar@gmail.com>
2021-05-24 12:39:14 +0530

    library updated and superpro callbacks added

--- app/src/main/java/com/example/superprolibtest/MainActivity.kt
+++ app/src/main/java/com/example/superprolibtest/MainActivity.kt
@@ -1,59 +1,53 @@
 package com.example.superprolibtest
 
-import android.content.Intent
+//import com.androidapp.videocalllib.utils.AppConstants.IntentConstants
+import android.content.Context
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
-import com.androidapp.videocalllib.ui.loading.LoadingActivity
-import com.androidapp.videocalllib.utils.AppConstants.IntentConstants
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import com.androidapp.videocalllib.ui.loading.SuperProActivity
+import com.androidapp.videocalllib.ui.loading.SuperProCallListener
+import com.example.superprolibtest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : SuperProActivity(), SuperProCallListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        binding.btnJoinCall.setOnClickListener { view ->
+            val meetingId = binding.edittext1.text.toString()
+            val token = binding.edittext2.text.toString()
 
-        val editText1 = findViewById<EditText>(R.id.edittext1)
-        val editText2 = findViewById<EditText>(R.id.edittext2)
+            val imm: InputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(binding.root.windowToken, 0)
 
-        //test values//
-//        editText1.setText("022-2355-348")
-//        editText2.setText("f25bb5da-4d4f-4c37-a62d-416702228abf")
-
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            val meetingId = editText1.text.toString()
-            val token = editText2.text.toString()
             if(meetingId.isNotEmpty() && token.isNotEmpty()){
-                startSuperproCall(meetingId,token)
+                setCallFrame(binding.callFrame)
+                startCall(meetingId, token, this@MainActivity)
+            } else {
+                Toast.makeText(this@MainActivity,"MeetingId or token is invalid.",Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    fun startSuperproCall(meetingId: String, token: String) {
-        val intent = Intent(this, LoadingActivity::class.java)
-        intent.putExtra(IntentConstants.MEETING_ID, meetingId)
-        intent.putExtra(IntentConstants.TOKEN, token)
-        startActivity(intent)
+    override fun audioStatusChange(isEnabled: Boolean) {
+        val text = if(isEnabled) "Audio Enabled" else "Audio Disabled"
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    override fun callStatusChange(status: String) {
+        Toast.makeText(this, status, Toast.LENGTH_SHORT).show()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    override fun videoStatusChange(isEnabled: Boolean) {
+        val text = if(isEnabled) "Video Enabled" else "Video Disabled"
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
